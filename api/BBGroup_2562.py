cda39bbcb527c1f7d47d99c6a31b4e065e09a562
NNAm <42909578+NETCHANOK61@users.noreply.github.com>
2020-12-15 13:56:44 +0700

    Ver2.1

---
+++ api/BBGroup_2562.py
@@ -0,0 +1,26 @@
+import requests
+from pprint import pprint
+import pygal
+def SearchProject():
+    url = 'https://opend.data.go.th/govspending/bb_group_by_aspect'
+    token = 'A9VPOilnDeOorteHHvLCclobveFpOnOa'
+    parameters = {'api-key':token, 
+                'year':2562,
+                }
+    data = requests.get(url,params=parameters)
+    doc = data.json()
+    allproject = doc['result']
+    print(len(allproject))
+    list_name = []
+    list_amount = []
+    for i in allproject:
+        list_name.append(i['name'])
+        list_amount.append(i['amount'])
+    print(list_name)
+    print(list_amount)
+    pie_chart = pygal.Pie(legend_at_bottom=True, legend_at_bottom_columns=1)
+    pie_chart.title = 'ปีงบประมาณ พ.ศ. 2562'
+    for i in range (len(list_amount)):
+        pie_chart.add(list_name[i], int(list_amount[i]))
+    pie_chart.render_to_file('api/SVG/chart_BBGroup_2562.svg')
+SearchProject()
